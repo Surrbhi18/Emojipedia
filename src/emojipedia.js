5322d9e6d8516235de228c4f1907be99e96a5cff
Surabhi Gupta <96222130+Surrbhi18@users.noreply.github.com>
2022-02-22 11:05:38 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,39 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "😄",
+    name: "Grinning Face with Smiling Eyes",
+    meaning:
+      "A yellow face with smiling eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general happiness and good-natured amusement."
+  },
+  {
+    id: 2,
+    emoji: "😵‍💫",
+    name: "Face with Spiral Eyes",
+    meaning:
+      "A face showing a spiral eyes, commonly used to indicate dizziness, but may also represent hypnosis or other forms of disorientation."
+  },
+  {
+    id: 3,
+    emoji: "🤪",
+    name: "Zany Face",
+    meaning:
+      "A yellow face with its head tilted, its tongue hanging out of a big grin, and wide eyes in a wild, cockeyed expression. Generally used to express silliness."
+  },
+  // {
+  //   id: 4,
+  //   emoji: "🤓",
+  //   name: "Nerd Face",
+  //   meaning:
+  //     "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  // },
+  {
+    id: 4,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments."
+  }
+];
+
+export default emojipedia;
